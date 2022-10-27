f8d7e2c984067733fbea9c596b254938b7ad159c
jlnbnt <benat.julien@gmail.com>
2022-10-27 17:03:40 +0200

    apollo update

--- src/graphql/apollo.js
+++ src/graphql/apollo.js
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-const API_URL = "http://inrs.local/graphql";
+const API_URL = "https://inrs.epizy.com/graphql";
 
 const client = new ApolloClient({
   uri: API_URL,
